bed0025ea520d581e041f4cc5804275a42efa7c1
gr-hemanth <grhemanth44@gmail.com>
2025-12-20 18:23:38 +0530

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,7 @@
+document.querySelector('#app').innerHTML = `
+  <div class="container">
+    <h1 class="title">TRACK_IT</h1>
+    <button id="action-btn" class="action-btn">Click Me</button>
+  </div>
+`
+
